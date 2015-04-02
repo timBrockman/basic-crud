ba85ccb0401b7263367d5ca10ee930e91be5ee8b
timBrockman <tim@timbrockman.com>
2015-04-02 15:08:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# q/d express + mongoskin RESTful api project
+this is still raw..
+
+todo: share on github for easy access
+todo: everything but failing tests
+todo: a little extra validation or formatting
+todo: better organization (more named functions, less banana)
+todo: consider using config file for db and api key stuff (more github friendly)
+todo: consider mongolabs or mongohq in addition to local
---
+++ server.test.js
@@ -0,0 +1,80 @@
+// some q&d behavior checks to start with
+
+// reqs
+var superagent  = require('superagent');
+var expect      = require('expect.js');
+
+// tests will use superagent to post put get delete
+describe('Simple RESTful api server.. ', function(){
+  var id;//will be set by post and used for later tests
+  it('should create some object', function(done){
+    //post object with superagent
+    superagent.post('http://localhost:8080/api/test')
+      .send({
+        name  : 'foo',
+        job   : 'bartender',
+        email : 'foo@bar.com'
+      })
+      .end(function(err,res){
+        //no errs response ok and object
+        expect(err).to.eql(null);
+        expect(res).to.be.ok();
+        expect(res.body).to.be.an('object');
+        expect(res.body[0]._id.length).to.eql(24);//mongo id hex value
+        //set id for other tests
+        id = res.body[0]._id;
+        done();
+      });
+  });
+  it('should retrieve a single object', function(done){
+    superagent.get('http://localhost:8080/api/test/' + id)
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.body).to.be.an('object');
+        expect(res.body.name).to.eql('foo');
+        expect(res.body._id).to.eql(id);
+        done();
+      });
+  });
+  it('should retrieve a list of stuff', function(done){
+    superagent.get('http://localhost:8080/api/test')
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+  });
+  it('should update some object', function(done){
+    superagent.put('http://localhost:8080/api/test' + id)
+      .send({
+        name  : 'fu',
+        job   : 'barrister',
+        email : 'fu@barrister.com'
+      })
+      .end(function(err,res){
+        expect(err).to.eql(null);
+        expect(res.body).to.be.an('object');
+        expect(res.body.msg).to.eql('success');
+        done();
+      });
+  });
+  it('shoud check updated object', function(done){
+    superagent.get('http://localhost:8080/api/test' + id)
+      .end(function(err, res){
+        expect(err).to.eql(null);
+        expect(res.body).to.be.an('object');
+        expect(res.body._id).to.eql(id);
+        expect(res.body.name).to.eql('fu');
+        done();
+      });
+  });
+  it('should delete some object', function(done){
+    superagent.del('http://localhost:8080/api/test' + id)
+      .end(function(err,res){
+        expect(err).to.eql(null);
+        expect(res.body).to.be.an('object');
+        expect(res.body.msg).to.eql('success');
+        done();
+      });
+  });
+});
